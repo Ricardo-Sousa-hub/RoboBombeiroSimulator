bf3a901e91d61c4a0b55d08fa747bc7bbcdbcc12
Ricardo <satao.net@hotmail.com>
2023-01-01 18:22:22 +0000

    Reder.

--- src/Display.java
+++ src/Display.java
@@ -1,11 +1,45 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Display extends Canvas {
+public class Display extends Canvas implements Runnable {
 
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 600;
-    public static final String TITLE = "Robo Bombeiro Simulator";
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    private static final String TITLE = "Robo Bombeiro Simulator";
+    private Thread thread;
+    private boolean isRunning = false;
+    private Render render;
+
+    public Display(){
+        render = new Render(WIDTH, HEIGHT);
+    }
+
+    public void Start(){
+        if(!isRunning){
+            isRunning = true;
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    public void Stop(){
+        if(isRunning){
+            isRunning = false;
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void tick(){
+
+    }
+
+    private void render(){
+
+    }
 
     public static void main(String[] args) {
         Display simulador = new Display();
@@ -20,5 +54,12 @@ public class Display extends Canvas {
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void run() {
+        while (isRunning){
+
+        }
     }
 }
