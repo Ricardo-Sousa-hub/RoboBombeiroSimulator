9d2e07ef471b81873e1419e0345c53d5c11f1231
Ricardo <satao.net@hotmail.com>
2023-01-13 19:41:18 +0000

    Created orbit and JMenuBar.

--- src/RoboBombeiro/Display.java
+++ src/RoboBombeiro/Display.java
@@ -1,12 +1,17 @@
 package RoboBombeiro;
 
+import com.sun.j3d.audioengines.javasound.JavaSoundMixer;
+import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
 import com.sun.j3d.utils.picking.PickCanvas;
+import com.sun.j3d.utils.universe.SimpleUniverse;
 
-import javax.media.j3d.Appearance;
-import javax.media.j3d.Canvas3D;
-import javax.media.j3d.View;
+import javax.media.j3d.*;
 import javax.swing.*;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
 public class Display extends JFrame {
@@ -22,15 +27,17 @@ public class Display extends JFrame {
     private Canvas3D cv;
     private Appearance lit = new Appearance();
 
-    public void Start(){
-        if(!isRunning){
+    BoundingSphere bounds = new BoundingSphere();
+
+    public void Start() {
+        if (!isRunning) {
             isRunning = true;
 
         }
     }
 
-    public void Stop(){
-        if(isRunning){
+    public void Stop() {
+        if (isRunning) {
             isRunning = false;
         }
     }
@@ -54,5 +61,59 @@ public class Display extends JFrame {
         }
     }
 
+    public Display() {
 
+        GraphicsConfiguration gc = SimpleUniverse.getPreferredConfiguration();
+        Canvas3D cv = new Canvas3D(gc);
+
+        SimpleUniverse su = new SimpleUniverse(cv, 2);
+        su.getViewingPlatform().setNominalViewingTransform();
+
+        setLayout(new BorderLayout());
+        add(cv, BorderLayout.CENTER);
+
+        BranchGroup bg = createSceneGraph(su.getViewingPlatform().getMultiTransformGroup().getTransformGroup(0));
+        bg.compile();
+
+        su.getViewingPlatform().setNominalViewingTransform();
+
+        su.addBranchGraph(bg);
+
+        Transform3D viewTr = new Transform3D();
+
+        viewTr.lookAt(new Point3d(-1, 2, 0.5), new Point3d(0, 0, 0), new Vector3d(0, 1, 0));
+        viewTr.invert();
+
+        su.getViewingPlatform().getViewPlatformTransform().setTransform(viewTr);
+
+        OrbitBehavior orbit = new OrbitBehavior(cv);
+        orbit.setSchedulingBounds(bounds);
+
+        su.getViewingPlatform().setViewPlatformBehavior(orbit);
+
+        AudioDevice audioDevice = new JavaSoundMixer(su.getViewer().getPhysicalEnvironment());
+        audioDevice.initialize();
+
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu menuOption = new JMenu("Opções");
+
+        JMenuItem menuItem = new JMenuItem("Sair");
+        menuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        menuBar.add(menuOption);
+        menuOption.add(menuItem);
+        setJMenuBar(menuBar);
+    }
+
+    private BranchGroup createSceneGraph(TransformGroup tgView) {
+        BranchGroup root = new BranchGroup();
+
+        return root;
+    }
 }
